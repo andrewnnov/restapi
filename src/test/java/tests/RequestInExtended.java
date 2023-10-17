25ff2e1bff10c55ecac0615bbaa30d8b36383324
andrewnnov <andrewnnov@yandex.ru>
2023-10-17 13:59:24 -0300

    add lombok

--- src/test/java/tests/RequestInExtended.java
+++ src/test/java/tests/RequestInExtended.java
@@ -1,8 +1,9 @@
 package tests;
 
-import io.restassured.http.ContentType;
-import model.LoginBodyModel;
-import model.LoginResponseModel;
+import model.lombok.LoginBodyLombokModel;
+import model.lombok.LoginResponseLombokModel;
+import model.pojo.LoginBodyModel;
+import model.pojo.LoginResponseModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -51,7 +52,28 @@ public class RequestInExtended {
 
         Assertions.assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
         assertThat(loginResponse.getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
+    }
 
+    @Test
+    void loginTestLombok() {
+        LoginBodyLombokModel bodyModel = new LoginBodyLombokModel();
+        bodyModel.setEmail("eve.holt@reqres.in");
+        bodyModel.setPassword("cityslicka");
 
+        LoginResponseLombokModel loginResponse = given()
+                .log().uri()
+                .contentType(JSON)
+                .log().body()
+                .body(bodyModel)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().as(LoginResponseLombokModel.class);
+
+        Assertions.assertEquals("QpwL5tke4Pnpja7X4", loginResponse.getToken());
+        assertThat(loginResponse.getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
     }
 }
